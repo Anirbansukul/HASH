1af1dbbc7f8b42fbcca6fae8d495361c5a4c16f8
ANIRBAN SUKUL <125815679+Anirbansukul@users.noreply.github.com>
2024-03-02 21:14:37 +0530

    Add files via upload

---
+++ Q4.java
@@ -0,0 +1,15 @@
+package HashMap;
+import java.util.*;
+public class Q4 {
+
+	public static void main(String[] args) {
+		LinkedHashMap<Integer,String> map=new LinkedHashMap<Integer,String>();
+		map.put(18,"Anirban");
+		map.put(50,"Pradip");
+		map.put(45,"Soma");
+		map.put(24,"Tithi");
+		map.put(1,"Diya");
+		map.put(28,"Rajib");
+		System.out.println(map);
+	}
+}
