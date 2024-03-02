45ddb6ecbbf65963f531fcc6e60856b694ae117d
ANIRBAN SUKUL <125815679+Anirbansukul@users.noreply.github.com>
2024-03-02 21:14:21 +0530

    Add files via upload

---
+++ Q3.java
@@ -0,0 +1,17 @@
+package HashMap;
+import java.util.*;
+public class Q3 {
+
+	public static void main(String[] args) {
+		TreeMap<Integer,String> tree=new TreeMap<Integer,String>();
+		tree.put(18,"Anirban");
+		tree.put(50,"Pradip");
+		tree.put(45,"Soma");
+		tree.put(24,"Tithi");
+		tree.put(1,"Diya");
+		tree.put(28,"Rajib");
+		System.out.println(tree);
+
+	}
+
+}
