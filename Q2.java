15126e739166924a6319dd2f72d47ae81e7c1bed
ANIRBAN SUKUL <125815679+Anirbansukul@users.noreply.github.com>
2024-03-02 21:14:05 +0530

    Add files via upload

---
+++ Q2.java
@@ -0,0 +1,16 @@
+package HashMap;
+import java.util.*;
+public class Q2 {
+
+	public static void main(String[] args) {
+		Hashtable<String,Integer> table=new Hashtable<String,Integer>();
+		table.put("Anirban",18);
+		table.put("Pradip",50);
+		table.put("Soma",45);
+		table.put("Tithi",24);
+		table.put("Diya",1);
+		table.put("Rajib",28);
+		System.out.println(table);
+	}
+
+}
