81a45c4d4f268edb1812917de88d67ebe68d121c
ANIRBAN SUKUL <125815679+Anirbansukul@users.noreply.github.com>
2024-03-02 21:13:42 +0530

    Add files via upload

---
+++ Q1.java
@@ -0,0 +1,28 @@
+package HashMap;
+import java.util.*;
+public class Q1 {
+	public static void main(String[] args) {
+		HashMap<String,Integer> map=new HashMap<String,Integer>();
+		map.put("Anirban",18);
+		map.put("Pradip",50);
+		map.put("Soma",45);
+		map.put("Tithi",24);
+		map.put("Diya",1);
+		map.put("Rajib",28);
+//	   System.out.println(map.remove("Rajib"));
+//	   System.out.println(map);
+		//System.out.println(map.containsKey("ANI"));
+		//System.out.println(map.containsValue(1));
+		//System.out.println(map.get("ANI"));
+	    //System.out.println(map.get("Anirban"));
+//		System.out.println(map.keySet());
+//		System.out.println(map.values());
+//		System.out.println(map.entrySet());
+//		Set set=map.entrySet();
+//		Iterator itr=set.iterator();
+//		while(itr.hasNext()) {
+//			Map.Entry nob=(Map.Entry)itr.next();
+//			System.out.println("Keys are:"+nob.getKey()+"\n"+"Values are:"+nob.getValue());
+//		}
+	}
+}
